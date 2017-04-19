91d083afaf183db8a77006d320c24ee81f58cb6d
Semen Makhaev <semenmakhaev@yandex.ru>
2017-04-20 00:26:10 +0500

    Функциональность реализована

--- getPokerHand.js
+++ getPokerHand.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Определение комбинации в броске
  *
@@ -5,9 +7,64 @@
  * @returns {String} название комбинации
  */
 function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
+    if (arguments.length !== 1 || !Array.isArray((dice)) || dice.length !== 5) {
+        throw new Error('Неверный аргумент');
+    }
+    const repetitions = getRepetitions(dice);
+    const maxCount = maxItem(repetitions);
+    switch (maxCount) {
+    case 5:
+        return 'Покер';
+    case 4:
+        return 'Каре';
+    case 3:
+        if (repetitions.contains(2)) {
+            return 'Фулл хаус';
+        }
+        return 'Тройка';
+    case 2:
+        if (repetitions.filter(item => {
+            item === 2;
+        }).length === 2) {
+            return 'Две пары';
+        }
+        return 'Пара';
+    default:
+        return 'Наивысшее очко';
+    }
+}
 
-    return 'Покер';
+/**
+ * Возвращает число повторений элементов массива
+ *
+ * @param {Array} arr
+ * @returns {Number}
+ */
+function getRepetitions(arr) {
+    return arr.reduce((repetitions, current) => {
+        if (current < 1 || current > 6) {
+            throw new Error('Неправильное значение');
+        }
+        repetitions[current-1]++;
+
+        return repetitions;
+    }, arr.map(() => {
+
+        return 0;
+    }));
+}
+
+/**
+ * Возвращает наибольший элемент массива
+ *
+ * @param {Array} arr
+ * @return {Number}
+ */
+function maxItem(arr) {
+    return arr.reduce((current, item) => {
+
+        return item > current ? item : current;
+    }, 0);
 }
 
 module.exports = getPokerHand;
