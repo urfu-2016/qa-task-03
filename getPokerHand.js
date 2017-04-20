4f5ae864837c74278147e214997608dcf5926a6b
nikitc <nikitc6@rambler.ru>
2017-04-20 14:05:51 +0500

    Добавлено решение

--- getPokerHand.js
+++ getPokerHand.js
@@ -1,3 +1,95 @@
+'use strict';
+
+Array.prototype.getCountIdentical = function (elem) {
+    return this.reduce((count, currentElem) => {
+        return count + ((currentElem === elem) ? 1 : 0);
+    }, 0);
+};
+
+function checkInput(dice) {
+    if (!Array.isArray(dice) || arguments.length != 1) {
+        throw new Error('Incorrect arguments');
+    }
+
+    if (dice.length !== 5) {
+        throw new Error('Invalid length array');
+    }
+
+    if (Math.max.apply(Math, dice) > 5 || Math.min.apply(Math, dice) < 1) {
+        throw new Error('Array contains incorrect values');
+    }
+}
+
+function getCombinations(dice) {
+    checkInput(dice);
+
+    var combinations = {
+        pair: 0,
+        set: 0,
+        single: 0
+    };
+    var pastCombinations = [];
+    for (var i = 0; i < dice.length; i++) {
+        if (pastCombinations.indexOf(dice[i]) !== -1) {
+            continue;
+        }
+        switch (dice.getCountIdentical(dice[i])) {
+            case 1:
+                combinations.single++;
+                pastCombinations.push(dice[i]);
+                break;
+            case 2:
+                combinations.pair++;
+                pastCombinations.push(dice[i]);
+                break;
+            case 3:
+                combinations.set++;
+                pastCombinations.push(dice[i]);
+                break;
+        }
+    }
+
+    return combinations;
+}
+
+function isPoker(dice) {
+    var firstCard = dice[0];
+
+    return dice.every((card) => {
+        return firstCard === card;
+    });
+}
+
+function isFourOfKind(dice) {
+    return (dice.getCountIdentical(dice[0]) === 4) ||
+        (dice.getCountIdentical(dice[1] === 4));
+}
+
+function isFullHouse(dice) {
+    var combinations = getCombinations(dice);
+
+    return combinations.pair === 1 && combinations.set === 1;
+}
+
+function isSet(dice) {
+    var combinations = getCombinations(dice);
+
+    return combinations.single === 2 && combinations.set === 1;
+}
+
+
+function isTwoPair(dice) {
+    var combinations = getCombinations(dice);
+
+    return combinations.pair === 2;
+}
+
+function isOnePair(dice) {
+    var combinations = getCombinations(dice);
+
+    return combinations.pair === 1 && combinations.single === 3;
+}
+
 /**
  * Определение комбинации в броске
  *
@@ -5,9 +97,30 @@
  * @returns {String} название комбинации
  */
 function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
+    if (isPoker(dice)) {
+        return 'Покер';
+    }
 
-    return 'Покер';
+    if (isFourOfKind(dice)) {
+        return 'Каре';
+    }
+
+    if (isFullHouse(dice)) {
+        return 'Фулл хаус';
+    }
+
+    if (isSet(dice)) {
+        return 'Тройка';
+    }
+
+    if (isTwoPair(dice)) {
+        return 'Две пары';
+    }
+
+    if (isOnePair(dice)) {
+        return 'Пара';
+    }
+
+    return 'Наивысшее очко';
 }
-
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -5,7 +5,71 @@ describe('getPokerHand', () => {
     it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
         const actual = getPokerHand([1, 1, 1, 1, 1]);
 
-        assert(actual, 'Покер');
+        assert.equal(actual, 'Покер');
+    });
+
+
+    it('should return `Каре` for [1, 1, 1, 1, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+    it('should return `Фулл хаус` for [1, 1, 1, 2, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 2]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+
+    it('should return `Тройка` for [1, 1, 1, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 3]);
+
+        assert.equal(actual, 'Тройка');
+    });
+
+    it('should return `Две пары` for [1, 1, 2, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 2, 2, 3]);
+
+        assert.equal(actual, 'Две пары');
+    });
+
+    it('should return `Пара` for [1, 1, 2, 3, 4]', () => {
+        const actual = getPokerHand([1, 1, 2, 3, 4]);
+
+        assert.equal(actual, 'Пара');
+    });
+
+    it('should return `Наивысшее очко` for [1, 2, 3, 4, 5]', () => {
+        const actual = getPokerHand([1, 2, 3, 4, 5]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
+
+
+    it('should throw error for array contains not equal five arguments', () => {
+        const error = () => getPokerHand([1, 2, 3, 4, 5, 5]);
+
+        assert.throws(error, 'Invalid length array');
+    });
+
+
+    it('should throw error for array contains incorrect values (more than 5)', () => {
+        const error = () => getPokerHand([1, 2, 3, 4, 7]);
+
+        assert.throws(error, 'Array contains incorrect values');
+    });
+
+    it('should throw error for array contains incorrect values (less 1)', () => {
+        const error = () => getPokerHand([1, 2, 3, 4, -1]);
+
+        assert.throws(error, 'Array contains incorrect values');
+    });
+
+    it('should throw error for incorrect arguments', () => {
+        const error = () => getPokerHand('Hi!');
+
+        assert.throws(error, 'Incorrect arguments');
     });
 
     // Напишите тесты на ваш замечательный код здесь
