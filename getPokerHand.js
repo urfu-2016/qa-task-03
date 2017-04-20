f5d69c949570a57eef024618b209ff4af1b583db
Gallyam Biktashev <gbiktashev@naumen.ru>
2017-04-20 13:34:44 +0500

    Implement getPokerHand

--- getPokerHand.js
+++ getPokerHand.js
@@ -6,8 +6,42 @@
  */
 function getPokerHand(dice) {
     // Напишите ваш замечательный код здесь
-
-    return 'Покер';
+    switch (calc(dice)) {
+        case 25:
+            return 'Покер';
+        case 17:
+            return 'Каре';
+        case 13:
+            return 'Фулл хаус';
+        case 11:
+            return 'Тройка';
+        case 9:
+            return 'Две пары';
+        case 7:
+            return 'Пара';
+        default:
+            return 'Наивысшее очко';
+    }
 }
+
+function calc(x) {
+    const res = [];
+    var current = x[0];
+    var y = 0;
+    for (var i = 0; i < x.length; i++) {
+        if (current !== x[i]) {
+            current = x[i];
+            res.push(y * y);
+            y = 0;
+        }
+        y++;
+    }
+    res.push(y * y);
+
+    return res.reduce(function (prev, curr) {
+        return prev + curr;
+    }, 0);
+}
+
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -8,5 +8,41 @@ describe('getPokerHand', () => {
         assert.equal(actual, 'Покер');
     });
 
+    it('should return `Каре` for [1, 1, 1, 1, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+    it('should return `Фулл хаус` for [1, 1, 1, 2, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 2]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+    it('should return `Тройка` for [1, 1, 1, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 3]);
+
+        assert.equal(actual, 'Тройка');
+    });
+
+    it('should return `Две пары` for [1, 1, 2, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 2, 2, 3]);
+
+        assert.equal(actual, 'Две пары');
+    });
+
+    it('should return `Пара` for [1, 1, 2, 4, 3]', () => {
+        const actual = getPokerHand([1, 1, 2, 4, 3]);
+
+        assert.equal(actual, 'Пара');
+    });
+
+    it('should return `Наивысшее очко` for [1, 5, 2, 4, 3]', () => {
+        const actual = getPokerHand([1, 5, 2, 4, 3]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
+
     // Напишите тесты на ваш замечательный код здесь
 });
