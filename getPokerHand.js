2f54c63a80df410a10620ecc438a3514ad3efa49
Ткач Даниил <Tkach@MacBook-Air-Tkac.local>
2017-04-21 01:28:08 +0500

    реализован функционал и запилины тесты

--- getPokerHand.js
+++ getPokerHand.js
@@ -1,3 +1,4 @@
+const combinations = ['Покер', 'Каре', 'Фулл хаус', 'Тройка', 'Две пары', 'Пара'];
 /**
  * Определение комбинации в броске
  *
@@ -5,9 +6,33 @@
  * @returns {String} название комбинации
  */
 function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
+    if (dice === null || dice === undefined)
+        return null;
 
-    return 'Покер';
+    if (!Object.prototype.toString.call(dice).match('object Array'))
+        throw new Error('Type error'); 
+    
+    const isElemsNum = dice.every(item => (Object.prototype.toString.call(item).match('object Number')) && !isNaN(item))
+    
+     if (!isElemsNum)
+        throw new Error('Type error'); 
+
+    if (dice.length < 5) 
+        return null
+
+    /** Так как без babel низя Object.values :( */
+    let countsElemHash = (dice.reduce((res, item) => {
+        res[item] = res[item] ? res[item] + 1 : 1;
+        return res
+    }, {}));
+    let countsElem = Object.keys(countsElemHash).map(item => countsElemHash[item]);
+    if (countsElem.includes(5)) return 'Покер';
+    else if (countsElem.includes(4)) return 'Каре';
+    else if (countsElem.includes(3) && countsElem.includes(2)) return 'Фулл хаус';
+    else if (countsElem.includes(3)) return 'Тройка';
+    else if (countsElem.includes(2) && countsElem.length === 3) return 'Две пары';
+    else if (countsElem.includes(2)) return 'Пара';
+    else return Math.max.apply(null, dice);
 }
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -2,11 +2,115 @@ const assert = require('assert');
 const getPokerHand = require('../getPokerHand');
 
 describe('getPokerHand', () => {
+    /** Положительные тесты */
     it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
         const actual = getPokerHand([1, 1, 1, 1, 1]);
 
         assert.equal(actual, 'Покер');
     });
 
-    // Напишите тесты на ваш замечательный код здесь
+    it('should return `Каре` for [5, 5, 1, 5, 5]', () => {
+        const actual = getPokerHand([5, 5, 1, 5, 5]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+    it('should return `Фулл хаус` for [12, 12, 100, 100, 100]', () => {
+        const actual = getPokerHand([12, 12, 100, 100, 100]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+    it('should return `Тройка` for [12, 13, -1, -1, -1]', () => {
+        const actual = getPokerHand([12, 13, -1, -1, -1]);
+
+        assert.equal(actual, 'Тройка');
+    });
+
+    it('should return `Две пары` for [9, 0, 2, 0, 2]', () => {
+        const actual = getPokerHand([9, 0, 2, 0, 2]);
+
+        assert.equal(actual, 'Две пары');
+    });
+
+    it('should return `4` for [-1, 0, 3, 4, 2]', () => {
+        const actual = getPokerHand([-1, 0, 3, 4, 2]);
+
+        assert.equal(actual, '4');
+    });
+
+    /** Негативные тесты */
+    it('should return `null` for []', () => {
+        const actual = getPokerHand([]);
+
+        assert.equal(actual, null);
+    });
+
+    /** Негативные тесты */
+    it('should return `null` for array which length < 5', () => {
+        const actual = getPokerHand([1,3,4]);
+
+        assert.equal(actual, null);
+    });
+
+    it('should return `null` for undefined', () => {
+        const actual = getPokerHand();
+
+        assert.equal(actual, null);
+    });
+
+    it('should return `Type error` for [1, 4, 3, `5`, 4]', () => {
+        try {
+            getPokerHand([1, 4, 3, '5', 4]);
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Type error');
+        }
+    });
+
+    it('should return `Type error` for [1, 4, 3, null]', () => {
+        try {
+            getPokerHand([1, 4, 3, null]);
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Type error');
+        }
+    });
+
+    it('should return `Type error` for [1, 4, 3, undefined]', () => {
+        try {
+            getPokerHand([1, 4, 3, undefined]);
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Type error');
+        }
+    });
+
+    it('should return `Type error` for [1, 4, 3, NaN]', () => {
+        try {
+            getPokerHand([1, 4, 3, NaN]);
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Type error');
+        }
+    });
+
+    it('should return `Type error` for {}', () => {
+        try {
+            getPokerHand({});
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Type error');
+        }
+    });
+
+    it('should return `Type error` for `string`' , () => {
+        try {
+            getPokerHand('string');
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Type error');
+        }
+    });
 });
+
