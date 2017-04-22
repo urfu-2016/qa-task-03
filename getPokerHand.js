df7e4c8b3f61903fca54b606f8ac2c18f97bc54c
SASHA-ASUS\Беляев <alexander_belyaev95@mail.ru>
2017-04-22 22:54:10 +0500

    Реализовал определение комбинаций

--- getPokerHand.js
+++ getPokerHand.js
@@ -5,9 +5,75 @@
  * @returns {String} название комбинации
  */
 function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
+    if (arguments.length !== 1) {
+        throw new Error('Функция принимает только один аргумент');
+    }
+    validateDice(dice);
+    const repetitionsNumber = getRepetitionsNumber(dice);
+    return getCombination(repetitionsNumber);
+}
 
-    return 'Покер';
+/**
+ * Проверка входных данных на корректность
+ *
+ * @param {Array} dice массив из пяти костей, который нужно проверить
+ */
+function validateDice(dice) {
+    if (!Array.isArray(dice)) {
+        throw new Error('Аргумент должен быть массивом');
+    }
+    if (dice.length !== 5) {
+        throw new Error('Размер массива должен быть равен 5');
+    }
+    dice.forEach((item) => {
+        if (typeof item !== 'number') {
+            throw new Error('Значение кости должно быть числом');
+        }
+        if (item < 1 || item > 6) {
+            throw new Error('Значение кости должно быть целым числом от 1 до 6');
+        }
+    });
+}
+
+/**
+ * Поиск количества одинаковых элементов в массиве
+ *
+ * @param {Array} array
+ * @returns {Array}
+ */
+function getRepetitionsNumber(array) {
+    let result = [0, 0, 0, 0, 0, 0];
+    array.forEach((item) => {
+        result[item-1]++;
+    });
+    return result;
+}
+
+/**
+ * Возвращает название комбинации
+ *
+ * @param {Array} repetitionsNumber массив количества повторяющихся элементов
+ * @returns {String} название комбинации
+ */
+function getCombination(repetitionsNumber) {
+    const maxRepetition = Math.max(...repetitionsNumber);
+    switch (maxRepetition) {
+        case 5:
+            return 'Покер';
+        case 4:
+            return 'Каре';
+        case 3:
+            if (repetitionsNumber.indexOf(2) != -1) {
+                return 'Фулл хаус';
+            }
+            return 'Тройка';
+        case 2:
+            return repetitionsNumber.filter((item) => {
+                return item === maxRepetition;
+            }).length === 2 ? 'Две пары' : 'Пара';
+        default:
+            return 'Наивысшее очко';
+    }
 }
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -8,5 +8,75 @@ describe('getPokerHand', () => {
         assert.equal(actual, 'Покер');
     });
 
-    // Напишите тесты на ваш замечательный код здесь
+    it('should return `Каре` for [1, 1, 1, 1, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+    it('should return `Фулл хаус` for [1, 1, 1, 3, 3]', () => {
+        const actual = getPokerHand([1, 1, 1, 3, 3]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+    it('should return `Тройка` for [3, 3, 3, 1, 5]', () => {
+        const actual = getPokerHand([3, 3, 3, 1, 5]);
+
+        assert.equal(actual, 'Тройка');
+    });
+
+    it('should return `Две пары` for [3, 3, 4, 4, 5]', () => {
+        const actual = getPokerHand([3, 3, 4, 4, 5]);
+
+        assert.equal(actual, 'Две пары');
+    });
+
+    it('should return `Пара` for [5, 5, 3, 1, 4]', () => {
+        const actual = getPokerHand([5, 5, 3, 1, 4]);
+
+        assert.equal(actual, 'Пара');
+    });
+
+    it('should return `Наивысшее очко` for [3, 4, 2, 1, 6]', () => {
+        const actual = getPokerHand([3, 4, 2, 1, 6]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
+
+    it('should throw error when number of arguments is more than one', () => {
+        const cb = () => getPokerHand([1, 1, 1, 1, 1], [1, 2, 3]);
+
+        assert.throws(cb, /Функция принимает только один аргумент/);
+    });
+
+    it('should throw error when dice is not array', () => {
+        const cb = () => getPokerHand("this is dice");
+
+        assert.throws(cb, /Аргумент должен быть массивом/);
+    });
+
+    it('should throw error when dice length is not equals five', () => {
+        const cb = () => getPokerHand([1, 1, 1, 1, 1, 1]);
+
+        assert.throws(cb, /Размер массива должен быть равен 5/);
+    });
+
+    it('should throw error when dice contains not a number', () => {
+        const cb = () => getPokerHand([1, 1, 1, 1, "NaN"]);
+
+        assert.throws(cb, /Значение кости должно быть числом/);
+    });
+
+    it('should throw error when dice contains element less than one', () => {
+        const cb = () => getPokerHand([1, 1, 1, 1, 0]);
+
+        assert.throws(cb, /Значение кости должно быть целым числом от 1 до 6/);
+    });
+
+    it('should throw error when dice contains element greater than six', () => {
+        const cb = () => getPokerHand([1, 1, 1, 1, 42]);
+
+        assert.throws(cb, /Значение кости должно быть целым числом от 1 до 6/);
+    });
 });
