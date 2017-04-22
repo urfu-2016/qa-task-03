38176cea6332206231db346cd31d6c8f4d3c77a4
fLa <flatra66@gmail.com>
2017-04-22 18:03:06 +0500

    Implement getPokerHand functionality with tests

--- getPokerHand.js
+++ getPokerHand.js
@@ -1,13 +1,50 @@
 /**
  * Определение комбинации в броске
  *
- * @param {Array} dice пять костей, для которых нужно определить комбинацию
+ * @param {Array} dices пять костей, для которых нужно определить комбинацию
  * @returns {String} название комбинации
  */
-function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
+function getPokerHand(dices) {
+  if (!Array.isArray(dices)) {
+    throw new Error('You must provide array as a first argument of function');
+  }
 
+  if (dices.length !== 5) {
+    throw new Error('Array must content 5 elements');
+  }
+
+  let countRepeatedHsh = dices.reduce((memo, dice) => {
+    if (!Number.isInteger(dice)) {
+      throw new Error('Dice mist be an integer');
+    }
+
+    if (dice > 6 || dice < 1) {
+      throw new Error('Dice can only have 1, 2, 3, 4, 5, 6 values');
+    }
+
+    memo[dice] = memo[dice] || 0
+    memo[dice]++
+
+    return memo
+   }, {});
+
+  let countRepeatedValues = Object.keys(countRepeatedHsh).map((item) => countRepeatedHsh[item]);
+
+  if (countRepeatedValues.includes(5)) {
     return 'Покер';
+  } else if (countRepeatedValues.includes(4)) {
+    return 'Каре';
+  } else if (countRepeatedValues.includes(3) && countRepeatedValues.includes(2)) {
+    return 'Фулл хаус';
+  } else if (countRepeatedValues.includes(3)) {
+    return 'Тройка';
+  } else if (countRepeatedValues.includes(2) && countRepeatedValues.length === 3) {
+    return 'Две пары';
+  } else if (countRepeatedValues.includes(2)) {
+    return 'Пара';
+  } else {
+    return Math.max(...dices);
+  }
 }
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -2,11 +2,69 @@ const assert = require('assert');
 const getPokerHand = require('../getPokerHand');
 
 describe('getPokerHand', () => {
-    it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
-        const actual = getPokerHand([1, 1, 1, 1, 1]);
+  it('should return `Error` for non array arguments', () => {
+    const actual = () => getPokerHand(null);
 
-        assert.equal(actual, 'Покер');
-    });
+    assert.throws(actual, 'You must provide array as a first argument of getPokerHand function');
+  })
 
-    // Напишите тесты на ваш замечательный код здесь
+  it('should return `Error` for array with incorrect length', () => {
+    const actual = () => getPokerHand([1, 2, 3, 4, 5, 6]);
+
+    assert.throws(actual, 'Dices array must content 5 elements');
+  })
+
+  it('should return `Error` for array with non integer dice values', () => {
+    const actual = () => getPokerHand([1, 2, 3, 4, undefined]);
+
+    assert.throws(actual, 'Dice mist be an integer');
+  })
+
+  it('should return `Error` for array with out of range dice values', () => {
+    const actual = () => getPokerHand([1, 2, 3, 4, -5]);
+
+    assert.throws(actual, 'Dice can only have 1, 2, 3, 4, 5, 6 values');
+  })
+
+  it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
+    const actual = getPokerHand([1, 1, 1, 1, 1]);
+
+    assert.equal(actual, 'Покер');
+  });
+
+  it('should return `Каре` for [1, 1, 1, 1, 2]', () => {
+    const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+    assert.equal(actual, 'Каре');
+  });
+
+  it('should return `Фулл хаус` for [1, 1, 1, 2, 2]', () => {
+    const actual = getPokerHand([1, 1, 1, 2, 2]);
+
+    assert.equal(actual, 'Фулл хаус');
+  });
+
+  it('should return `Тройка` for [1, 1, 1, 2, 3]', () => {
+    const actual = getPokerHand([1, 1, 1, 2, 3]);
+
+    assert.equal(actual, 'Тройка');
+  });
+
+  it('should return `Две пары` for [1, 1, 2, 2, 3]', () => {
+    const actual = getPokerHand([1, 1, 2, 2, 3]);
+
+    assert.equal(actual, 'Две пары');
+  });
+
+  it('should return `Пара` for [1, 1, 2, 3, 4]', () => {
+    const actual = getPokerHand([1, 1, 2, 3, 4]);
+
+    assert.equal(actual, 'Пара');
+  });
+
+  it('should return `5` for [1, 2, 3, 4, 5]', () => {
+    const actual = getPokerHand([1, 2, 3, 4, 5]);
+
+    assert.equal(actual, '5');
+  });
 });
