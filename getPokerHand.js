af1054a21ace519c01471080d48ca2ab77264dff
Provoker <groser95@gmail.com>
2017-04-23 22:59:53 +0500

    Решение задание

--- getPokerHand.js
+++ getPokerHand.js
@@ -5,9 +5,65 @@
  * @returns {String} название комбинации
  */
 function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
 
-    return 'Покер';
+    if (arguments.length !== 1) {
+        throw new Error("Функция getPokerHand принимает ровно один элемент")
+    }
+
+    if (!Array.isArray(dice)) {
+        throw new Error("Аргумент getPokerHand должен быть массивом")
+    }
+
+    if (dice.length !== 5) {
+        throw new Error("Аргумент getPokerHand должен быть массивом ровно из пяти элементов")
+    }
+
+    let pointCounts = getPointCountsList(dice);
+    pointCounts.sort((a, b) => b - a);
+
+    return getCombination(pointCounts);
+}
+
+/**
+ * Получения массива для каждого очка
+ *
+ * @param {Array} dice пять костей, для которых нужно определить комбинацию
+ * @returns {Array} массив количества очков, где индекс элемента соотвествует очку, а значение - сколько встречается
+ */
+function getPointCountsList(dice) {
+    let pointCounts = [0, 0, 0, 0, 0, 0];
+    dice.forEach((point) => {
+        if (!Number.isInteger(point)) {
+            throw new Error("Элементы массива должны быть целыми числами")
+        }
+        if (point < 1 || point > 6) {
+            throw new Error("Элементы массива должны содержать числа от 1 до 6")
+        }
+        pointCounts[point - 1]++;
+    });
+    return pointCounts;
+}
+
+
+/**
+ * Получение названия комбинации
+ *
+ * @param {Array} pointCounts массив количества очков
+ * @returns {String} название комбинации
+ */
+function getCombination(pointCounts) {
+    switch (pointCounts[0]) {
+        case 5:
+            return "Покер";
+        case 4:
+            return "Каре";
+        case 3:
+            return pointCounts[1] === 2 ? "Фулл хаус" : "Тройка";
+        case 2:
+            return pointCounts[1] === 2 ? "Две пары" : "Пара";
+        case 1:
+            return "Наивысшее очко"
+    }
 }
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -2,11 +2,93 @@ const assert = require('assert');
 const getPokerHand = require('../getPokerHand');
 
 describe('getPokerHand', () => {
-    it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
-        const actual = getPokerHand([1, 1, 1, 1, 1]);
 
-        assert.equal(actual, 'Покер');
+    describe('Positive test', () => {
+        it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
+            const actual = getPokerHand([1, 1, 1, 1, 1]);
+
+            assert.equal(actual, 'Покер');
+        });
+
+        it('should return `Каре` for [1, 1, 1, 2, 1]', () => {
+            const actual = getPokerHand([1, 1, 1, 2, 1]);
+
+            assert.equal(actual, 'Каре');
+        });
+
+        it('should return `Фулл хаус` for [6, 1, 6, 1, 1]', () => {
+            const actual = getPokerHand([6, 1, 6, 1, 1]);
+
+            assert.equal(actual, 'Фулл хаус');
+        });
+
+        it('should return `Тройка` for [4, 4, 1, 4, 2]', () => {
+            const actual = getPokerHand([4, 4, 1, 4, 2]);
+
+            assert.equal(actual, 'Тройка');
+        });
+
+        it('should return `Две пары` for [3, 4, 1, 4, 3]', () => {
+            const actual = getPokerHand([3, 4, 1, 4, 3]);
+
+            assert.equal(actual, 'Две пары');
+        });
+
+        it('should return `Пара` for [3, 4, 1, 5, 3]', () => {
+            const actual = getPokerHand([3, 4, 1, 5, 3]);
+
+            assert.equal(actual, 'Пара');
+        });
+
+        it('should return `Наивысшее очко` for [3, 4, 1, 5, 2]', () => {
+            const actual = getPokerHand([3, 4, 1, 5, 2]);
+
+            assert.equal(actual, 'Наивысшее очко');
+        });
     });
 
-    // Напишите тесты на ваш замечательный код здесь
+
+    describe('Negative test', () => {
+        it('should throw Error if less than one argument', () => {
+            const cb = () => getPokerHand();
+            assert.throws(cb, /Функция getPokerHand принимает ровно один элемент/);
+        });
+
+        it('should throw Error if more than one argument', () => {
+            const cb = () => getPokerHand([3, 4, 1, 4, 3], [3, 4, 1, 4, 3]);
+            assert.throws(cb, /Функция getPokerHand принимает ровно один элемент/);
+        });
+
+        it('should throw Error if argument not array', () => {
+            const cb = () => getPokerHand("3, 4, 1, 4, 3");
+            assert.throws(cb, /Аргумент getPokerHand должен быть массивом/);
+        });
+
+        it('should throw Error if argument array contain less than 5 arguments', () => {
+            const cb = () => getPokerHand([3, 4, 1, 4]);
+            assert.throws(cb, /Аргумент getPokerHand должен быть массивом ровно из пяти элементов/);
+        });
+
+        it('should throw Error if argument array contain more than 5 arguments', () => {
+            const cb = () => getPokerHand([3, 4, 1, 4, 3, 3]);
+            assert.throws(cb, /Аргумент getPokerHand должен быть массивом ровно из пяти элементов/);
+        });
+
+
+        it('should throw Error if argument array contain not integer value', () => {
+            const cb = () => getPokerHand([3, 4, '1', 4, 3]);
+            assert.throws(cb, /Элементы массива должны быть целыми числами/);
+        });
+
+        it('should throw Error if argument array contain element more 6', () => {
+            const cb = () => getPokerHand([3, 4, 1, 7, 3]);
+            assert.throws(cb, /Элементы массива должны содержать числа от 1 до 6/);
+        });
+
+        it('should throw Error if argument array contain element less 1', () => {
+            const cb = () => getPokerHand([3, 4, 0, 6, 3]);
+            assert.throws(cb, /Элементы массива должны содержать числа от 1 до 6/);
+        });
+
+    });
 });
