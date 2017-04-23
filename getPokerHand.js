fe1a97b31775f85431910eba80c303db667c34b7
Шапошникова Наталья <nata_9698@mail.ru>
2017-04-24 00:31:02 +0500

    Реализовала поведение комбинаций

--- getPokerHand.js
+++ getPokerHand.js
@@ -1,3 +1,6 @@
+const diceLength = 5;
+const correctValues = [1, 2, 3, 4, 5, 6];
+
 /**
  * Определение комбинации в броске
  *
@@ -5,9 +8,54 @@
  * @returns {String} название комбинации
  */
 function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
+    if (dice.length != diceLength) {
+        throw new Error(`Длина массива должна быть равна 5`)
+    }
+    if (dice.some(value => !(correctValues.includes(value)))) {
+        throw new RangeError('Значения должны быть целыми числами и иметь значения от 1 до 6');
+    }
+    dice.sort();
 
-    return 'Покер';
+    var arrMax = getMaxCount(dice);
+    switch (arrMax[0]) {
+        case 5 :
+            return 'Покер';
+        case 4:
+            return 'Каре';
+        case 3:
+            return arrMax[1] == 2 ? 'Фулл хаус' : 'Тройка';
+        case 2:
+            return arrMax[1] == 2 ? 'Две Пары' : 'Пара';
+        case 1:
+            return 'Наивысшее очко';
+    }
+}
+
+/**
+ * Определяет две максимальные последовательности повторяющихся цифр
+ *
+ * @param {Array} dice пять костей, для которых нужно определить максимальное повторение цифры
+ * @returns {Array} Массив из двух элементов.
+ */
+function getMaxCount(dice) {
+    var maxCount1 = 0;
+    var maxCount2 = 0;
+    var count = 1;
+    for (var i = 1; i <= dice.length; i++) {
+        if (dice[i - 1] != dice[i] || i == dice.length) {
+            if (count > maxCount1) {
+                maxCount2 = maxCount1;
+                maxCount1 = count;
+            }
+            else {
+                if (count > maxCount2)
+                    maxCount2 = count;
+            }
+            count = 1;
+        }
+        else count++;
+    }
+    return ([maxCount1, maxCount2]);
 }
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -8,5 +8,77 @@ describe('getPokerHand', () => {
         assert.equal(actual, 'Покер');
     });
 
-    // Напишите тесты на ваш замечательный код здесь
+    it('should return `Каре` for [1, 1, 2, 1, 1]', () => {
+        const actual = getPokerHand([1, 1, 2, 1, 1]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+    it('should return `Фулл хаус` for [1, 2, 1, 2, 1]', () => {
+        const actual = getPokerHand([1, 2, 1, 2, 1]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+    it('should return `Фулл хаус` for [1, 4, 4, 1, 4]', () => {
+        const actual = getPokerHand([1, 4, 4, 1, 4]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+    it('should return `Тройка` for [1, 6, 2, 6, 6]', () => {
+        const actual = getPokerHand([1, 6, 2, 6, 6]);
+
+        assert.equal(actual, 'Тройка');
+    });
+
+    it('should return `Две Пары` for [3, 4, 4, 1, 1]', () => {
+        const actual = getPokerHand([3, 4, 4, 1, 1]);
+
+        assert.equal(actual, 'Две Пары');
+    });
+
+    it('should return `Пара` for [6, 1, 3, 2, 6]', () => {
+        const actual = getPokerHand([6, 1, 3, 2, 6]);
+
+        assert.equal(actual, 'Пара');
+    });
+
+    it('should return `Наивысшее очко` for [4, 1, 3, 2, 6]', () => {
+        const actual = getPokerHand([4, 1, 3, 2, 6]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
+
+    it('Должен выкидывать ошибку, когда значения кубиков не входят в [1, 6]', () => {
+        const cb = () => getPokerHand([1, 2, 0, 10, 7]);
+
+        assert.throws(cb, /Значения должны быть целыми числами и иметь значения от 1 до 6/);
+    });
+
+    it('Должен выкидывать ошибку, когда значения кубиков не являются целыми числами', () => {
+        const cb = () => getPokerHand([1, 2, 3.5, 4.0, 6]);
+
+        assert.throws(cb, /Значения должны быть целыми числами и иметь значения от 1 до 6/);
+    });
+
+    it('Должен выкидывать ошибку, когда значения кубиков не являются целыми числами', () => {
+        const cb = () => getPokerHand([1, '2', 4, 5, 6]);
+
+        assert.throws(cb, /Значения должны быть целыми числами и иметь значения от 1 до 6/);
+    });
+
+    it('Должен выкидывать ошибку, когда количесвто бросаемых кубиков больше 5', () => {
+        const cb = () => getPokerHand([1, 2, 3, 5, 2, 6]);
+
+        assert.throws(cb, /Длина массива должна быть равна 5/);
+    });
+
+    it('Должен выкидывать ошибку, когда количесвто бросаемых кубиков меньше 5', () => {
+        const cb = () => getPokerHand([1, 2, 3, 5]);
+
+        assert.throws(cb, /Длина массива должна быть равна 5/);
+    });
+
 });
+
