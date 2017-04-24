c15e4c65aa6b3858cdddfe09be42557ee17928e4
Fedor <mydearfrodo@yandex.ru>
2017-04-24 11:41:04 +0500

    fix ;

--- getPokerHand.js
+++ getPokerHand.js
@@ -30,33 +30,33 @@ function getCombinationType(firstMax, secondMax) {
 
 function getPokerHand(dice) {
     if (arguments.length != 1) {
-        throw new Error("Function getPokerHand take 1 argument")
+        throw new Error("Function getPokerHand take 1 argument");
     }
 
     if (!(dice instanceof Array)) {
-        throw new Error("Argument must be Array")
+        throw new Error("Argument must be Array");
     }
     
     if (dice.length !== 5) {
-        throw new Error("Dice should contain 5 elements")
+        throw new Error("Dice should contain 5 elements");
     }
 
     dice.forEach(card => {
         if (!Number.isInteger(card)) {
-            throw new Error("Dice array must contain only integer numbers numbers")
+            throw new Error("Dice array must contain only integer numbers numbers");
         }
         if (card < 1 || card > 6) {
-            throw new Error("Dice array must contain only numbers between 1 and 6")
+            throw new Error("Dice array must contain only numbers between 1 and 6");
         }
     })
     
-    var countCards = [0, 0, 0, 0, 0]
+    var countCards = [0, 0, 0, 0, 0];
     dice.forEach(card => {
-        countCards[card - 1] += 1
+        countCards[card - 1] += 1;
     })
-    countCards.sort((a, b) => b - a)
+    countCards.sort((a, b) => b - a);
 
-    return getCombinationType(countCards[0], countCards[1])
+    return getCombinationType(countCards[0], countCards[1]);
 }
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -2,37 +2,37 @@ const assert = require('assert');
 const getPokerHand = require('../getPokerHand');
 
 function getPokerHandError(errRegExp, ...args) {
-    const f = () => args[0] == undefined ? getPokerHand() : getPokerHand(...args)
+    const f = () => args[0] == undefined ? getPokerHand() : getPokerHand(...args);
     assert.throws(f, errRegExp);
 }
 
 describe('getPokerHand errors', () => {
     it('should throw error for empty arguments length', () => {
-        getPokerHandError(/Function getPokerHand take 1 argument/, undefined)
+        getPokerHandError(/Function getPokerHand take 1 argument/, undefined);
     });
     it('should throw error for more than 1 argument', () => {
-        getPokerHandError(/Function getPokerHand take 1 argument/, 1, 2)
+        getPokerHandError(/Function getPokerHand take 1 argument/, 1, 2);
     });
     it('should throw error if argument not Array', () => {
-        getPokerHandError(/Argument must be Array/, 'fake')
+        getPokerHandError(/Argument must be Array/, 'fake');
     });
     it('should throw error if dice contain less than 5 numbers', () => {
-        getPokerHandError(/Dice should contain 5 elements/, [1, 2, 3, 4, 5, 3])
+        getPokerHandError(/Dice should contain 5 elements/, [1, 2, 3, 4, 5, 3]);
     });
     it('should throw error if dice contain less more 5 numbers', () => {
-        getPokerHandError(/Dice should contain 5 elements/, [1, 3])
+        getPokerHandError(/Dice should contain 5 elements/, [1, 3]);
     });
     it('should throw error if dice contain not integer value', () => {
         getPokerHandError(/Dice array must contain only integer numbers numbers/,
-          [1, 3, 4.1, 3, 1])
+          [1, 3, 4.1, 3, 1]);
     });
     it('should throw error if dice contain integer value less than 1', () => {
         getPokerHandError(/Dice array must contain only numbers between 1 and 6/,
-          [1, 3, 0, 3, 1])
+          [1, 3, 0, 3, 1]);
     });
     it('should throw error if dice contain integer value more than 5', () => {
         getPokerHandError(/Dice array must contain only numbers between 1 and 6/,
-          [1, 3, 10, 3, 1])
+          [1, 3, 10, 3, 1]);
     });
 });
 describe('getPokerHand', () => {
