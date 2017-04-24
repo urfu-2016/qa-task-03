d30e204b8797e671055c65e5d9009aa76169642c
Artem Borzov <artem.borz@yandex.ru>
2017-04-24 21:34:50 +0500

    Добавил проверку сообщения об ошибке в тестах

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -13,40 +13,56 @@ function createCase(currentDice, expectedHand) {
 }
 
 describe('getPokerHand', () => {
-    it('should throws if dice length > 5', () => {
-        assert.throws(() => getPokerHand([1, 2, 3, 4, 5, 6]));
-    });
+    describe('incorrect dice length tests', () => {
+        let errorPattern = /dice should has five numbers/;
 
-    it('should throws if dice length < 5', () => {
-        assert.throws(() => getPokerHand([1, 2, 3, 4]));
-    });
+        it('should throws if dice length > 5', () => {
+            assert.throws(() => getPokerHand([1, 2, 3, 4, 5, 6]), errorPattern);
+        });
 
-    it('should throws if dice contains number > 6', () => {
-        assert.throws(() => getPokerHand([7, 1, 1, 1, 1]));
-    });
-
-    it('should throws if dice contains number < 1', () => {
-        assert.throws(() => getPokerHand([0, 1, 1, 1, 1]));
-    });
-
-    [0.38, NaN, 'somestring', {}].forEach(notInteger => {
-        it('should throws if dice contains not integer: ' + notInteger, () => {
-            assert.throws(() => getPokerHand([notInteger, 1, 1, 1, 1]));
+        it('should throws if dice length < 5', () => {
+            assert.throws(() => getPokerHand([1, 2, 3, 4]), errorPattern);
         });
     });
 
-    const handTestCases = [
-        createCase([1, 1, 1, 1, 1], Hand.POKER),
-        createCase([1, 1, 1, 1, 6], Hand.QUADS),
-        createCase([1, 2, 1, 2, 1], Hand.FULL_HOUSE),
-        createCase([1, 1, 2, 3, 1], Hand.SET),
-        createCase([3, 1, 3, 1, 6], Hand.TWO_PAIR),
-        createCase([6, 4, 3, 2, 6], Hand.PAIR),
-        createCase([1, 2, 3, 4, 6], Hand.HIGH_NUMBER)
-    ];
-    handTestCases.forEach(test => {
-        it(`should return '${test.expectedHand}' on '${test.currentDice}'`, () => {
-            assert.equal(getPokerHand(test.currentDice), test.expectedHand);
+    describe('incorrect number in dice tests', () => {
+        let errorPattern =  /dice should contains only numbers in range 1 to 6 inclusive/;
+
+        it('should throws if dice contains number > 6', () => {
+            assert.throws(() => getPokerHand([7, 1, 1, 1, 1]), errorPattern);
         });
+
+        it('should throws if dice contains number < 1', () => {
+            assert.throws(() => getPokerHand([0, 1, 1, 1, 1]), errorPattern);
+        });
+    });
+
+    describe('not integer in dice tests', () => {
+        let errorPattern = /dice should contains only integer numbers/;
+        let notIntegers = [0.38, NaN, 'somestring', {}]
+        
+        notIntegers.forEach(notInteger => {
+            it(`should throws if dice contains not integer: ${notInteger}`, () => {
+                assert.throws(() => getPokerHand([notInteger, 1, 1, 1, 1]), errorPattern);
+            });
+        });
+    });
+
+    describe('combinations tests', () => {
+        const handTestCases = [
+            createCase([1, 1, 1, 1, 1], Hand.POKER),
+            createCase([1, 1, 1, 1, 6], Hand.QUADS),
+            createCase([1, 2, 1, 2, 1], Hand.FULL_HOUSE),
+            createCase([1, 1, 2, 3, 1], Hand.SET),
+            createCase([3, 1, 3, 1, 6], Hand.TWO_PAIR),
+            createCase([6, 4, 3, 2, 6], Hand.PAIR),
+            createCase([1, 2, 3, 4, 6], Hand.HIGH_NUMBER)
+        ];
+        
+        handTestCases.forEach(test => {
+            it(`should return '${test.expectedHand}' on '${test.currentDice}'`, () => {
+                assert.equal(getPokerHand(test.currentDice), test.expectedHand);
+            });
+        });  
     });
 });
