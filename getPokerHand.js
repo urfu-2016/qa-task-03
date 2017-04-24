b3f7e17bd07193a6f0e770e7549f1a038ad30ec9
eoL95 <bragin.egor.95@gmail.com>
2017-04-24 22:09:51 +0500

    initial commit

--- getPokerHand.js
+++ getPokerHand.js
@@ -5,9 +5,51 @@
  * @returns {String} название комбинации
  */
 function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
+    if (!(dice instanceof  Array)) {
+        throw new Error('В функцию передан не массив');
+    }
 
-    return 'Покер';
+    if (dice.length !== 5) {
+        throw new Error('Длина массива не равна пяти');
+    }
+
+    dice.forEach((num, index) => {
+        let isAppropriateNumber = (num >= 1 && num <= 6);
+
+        if (!Number.isInteger(num)) {
+            throw new Error(`Под индексом '${index}' лежит не целое число`)
+        }
+
+        if (!isAppropriateNumber) {
+            throw new Error(`Число под индексом '${index}' лежит не в диапазоне 1-6`);
+        }
+    });
+
+    let countOfNumbers = [0, 0, 0, 0, 0];
+    let inArray = num => countOfNumbers.indexOf(num) !== -1;
+
+    dice.forEach((num) => {
+        countOfNumbers[num - 1]++;
+    });
+
+
+    let isTwoPair = inArray(2) &&
+        countOfNumbers.indexOf(2, countOfNumbers.indexOf(2) + 1) !== -1;
+    let combinations = {
+        'Покер': inArray(5),
+        'Каре': inArray(4),
+        'Фулл Хаус': inArray(3) && inArray(2),
+        'Тройка': inArray(3),
+        'Две пары': isTwoPair,
+        'Пара': inArray(2),
+        'Наивысшее очко': inArray(1)
+    };
+
+    for (let key in combinations) {
+        if (combinations[key]) {
+            return key;
+        }
+    }
 }
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -8,5 +8,84 @@ describe('getPokerHand', () => {
         assert.equal(actual, 'Покер');
     });
 
-    // Напишите тесты на ваш замечательный код здесь
+    it('should return `Каре` for [1, 1, 1, 1, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+    it('should return `Фулл Хаус` for [1, 1, 1, 2, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 2]);
+
+        assert.equal(actual, 'Фулл Хаус');
+    });
+
+    it('should return `Тройка` for [1, 1, 1, 2, 4]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 4]);
+
+        assert.equal(actual, 'Тройка');
+    });
+
+    it('should return `Две пары` for [2, 3, 3, 4, 4]', () => {
+        const actual = getPokerHand([2, 3, 3, 4, 4]);
+
+        assert.equal(actual, 'Две пары');
+    });
+
+    it('should return `Пара` for [2, 3, 3, 4, 5]', () => {
+        const actual = getPokerHand([2, 3, 3, 4, 5]);
+
+        assert.equal(actual, 'Пара');
+    });
+
+    it('should return `Наивысшее очко` for [1, 2, 3, 4, 5]', () => {
+        const actual = getPokerHand([1, 2, 3, 4, 5]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
+
+    it('should throw Error when getPokerHand argument is not Array', () => {
+        try {
+            getPokerHand(121);
+            throw new Error('`getPokerHand` should throw error');
+        } catch (err) {
+            assert.equal(err.message, 'В функцию передан не массив');
+        }
+    });
+
+    it('should throw Error when getPokerHand argument.length !== 5', () => {
+        try {
+            getPokerHand([1, 2]);
+            throw new Error('`getPokerHand` should throw error');
+        } catch (err) {
+            assert.equal(err.message, 'Длина массива не равна пяти');
+        }
+    });
+
+    it('should throw Error when there are not Integer elements in Array', () => {
+        try {
+            getPokerHand(['1', 2, 3, 4, 5]);
+            throw new Error('`getPokerHand` should throw error');
+        } catch (err) {
+            assert.equal(err.message, `Под индексом '0' лежит не целое число`);
+        }
+    });
+
+    it('should throw Error when there are elements more then 6', () => {
+        try {
+            getPokerHand([1, 7, 2, 3, 4]);
+            throw new Error('`getPokerHand` should throw error');
+        } catch (err) {
+            assert.equal(err.message, `Число под индексом '1' лежит не в диапазоне 1-6`);
+        }
+    });
+
+    it('should throw Error when there are elements less then 1', () => {
+        try {
+            getPokerHand([1, 0, 2, 3, 4]);
+            throw new Error('`getPokerHand` should throw error');
+        } catch (err) {
+            assert.equal(err.message, `Число под индексом '1' лежит не в диапазоне 1-6`);
+        }
+    });
 });
