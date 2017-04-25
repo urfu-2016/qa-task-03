dd210c17881e29e9b16518305e983ad06f96e0a4
NeSmogPridymatNick <IceDruid@yandex.ru>
2017-04-25 11:57:39 +0500

    Задача

--- getPokerHand.js
+++ getPokerHand.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * Определение комбинации в броске
  *
@@ -6,8 +7,51 @@
  */
 function getPokerHand(dice) {
     // Напишите ваш замечательный код здесь
-
-    return 'Покер';
+    var arrayDice = Object.seal([0, 0, 0, 0, 0 ,0, 0]);
+    for(var i = 0; i < 5; i++) {
+        try {
+            if ((dice.length > 5) || (dice[i] > 6)) {
+                throw new UserException('Смухлевать решил?')
+            }
+            arrayDice[dice[i]]++;
+        } catch(e) {
+            return 'Смухлевать решил?'; 
+        }
+    }
+    var combination = 'none';
+    var setDice = new Set();
+    var royalFlush = 0;
+    for(var i = 1; i < arrayDice.length; i++) {
+        if ((setDice.has(arrayDice[i])) && (arrayDice[i] === 2)) {
+            return ('Две пары');
+        } else {
+            setDice.add(arrayDice[i]);
+        }
+        if (arrayDice[i] === 1) {
+            royalFlush++;
+        } else if (royalFlush < 5) {
+            royalFlush = 0;
+        }
+    }
+    if (royalFlush === 5) {
+        return 'Наивысшее очко!'
+    }
+    if (setDice.has(3) && setDice.has(2)) {
+        return 'Фулл хаус';
+    }
+    if (setDice.has(4)) {
+        return 'Каре';
+    }
+    if (setDice.has(5)) {
+        return 'Покер';
+    }
+    if (setDice.has(2)) {
+        return 'Пара';
+    }
+    if (setDice.has(3)) {
+        return 'Тройка';
+    }
+    return 'Увы ничего'
 }
 
 module.exports = getPokerHand;
