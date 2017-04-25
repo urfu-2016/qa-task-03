cc9fa0e3efe57c39fa9cfd02aa3fc4961f9ab6e0
Vole1 <studying48@gmail.com>
2017-04-25 23:57:05 +0500

    Реализовал определение комбинаций

--- getPokerHand.js
+++ getPokerHand.js
@@ -7,7 +7,47 @@
 function getPokerHand(dice) {
     // Напишите ваш замечательный код здесь
 
-    return 'Покер';
+    
+    var indexesOfEq = [];
+    
+    var iterCounter = 0
+    for (var i = 0; i < dice.length; i++) {
+    	if (indexesOfEq[0] != undefined && indexesOfEq[0].includes(i))
+    		continue;
+    	if (indexesOfEq[1] != undefined && indexesOfEq[1].includes(i))
+    		continue;
+
+    	for (var j = i+1; j < dice.length; j++) {
+
+    		if(dice[i] == dice[j]){
+                if (indexesOfEq[iterCounter] == undefined)
+                    indexesOfEq[iterCounter] = [];
+    			indexesOfEq[iterCounter].push(j);
+            }
+
+     	};
+     	if (indexesOfEq[iterCounter] != undefined && indexesOfEq[iterCounter].length != 0)
+     		iterCounter++;
+    };
+
+    if (indexesOfEq[0] == undefined)
+    	return 'Наивысшее очко';
+    else if (indexesOfEq[1] == undefined || indexesOfEq[1].length == 0) {
+	    if (indexesOfEq[0].length == 4)
+	 		return 'Покер';
+	 	else if (indexesOfEq[0].length == 3)
+	 		return 'Каре';
+	 	else if (indexesOfEq[0].length == 2)
+	 		return 'Тройка';
+	 	else 
+	 		return 'Пара';
+ 	}
+ 	else {
+	    if ((indexesOfEq[0].length == 2 && indexesOfEq[1].length == 1) || (indexesOfEq[0].length == 1 && indexesOfEq[1].length == 2))
+	    	return 'Фулл хаус';
+		else
+	    	return 'Две пары';
+	}
 }
 
 module.exports = getPokerHand;
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -8,5 +8,53 @@ describe('getPokerHand', () => {
         assert.equal(actual, 'Покер');
     });
 
+
+    it ('should return `Покер` for [2, 2, 2, 2, 2]', () =>{
+    	const actual = getPokerHand([2, 2, 2, 2, 2]);
+
+    	assert.equal(actual, 'Покер');
+    });
+
+    it ('should return `Каре` for [9, 9, 3, 9, 9]', () =>{
+    	const actual = getPokerHand([9, 9, 3, 9, 9]);
+
+    	assert.equal(actual, 'Каре');
+    });
+
+    it ('should return `Фулл хаус` for [8, 7, 8, 8, 7]', () =>{
+    	const actual = getPokerHand([8, 7, 8, 8, 7]);
+
+    	assert.equal(actual, 'Фулл хаус');
+    });
+    //it ('should return `Фулл хаус` for [8, 7, 8, 7, 7]', () =>{
+    //	const actual = getPokerHand([8, 7, 8, 7, 7]);
+
+    //	assert.equal(actual, 'Фулл хаус');
+    //});
+
+    it ('should return `Тройка` for [3, 1, 2, 1, 1]', () =>{
+    	const actual = getPokerHand([3, 1, 2, 1, 1]);
+
+    	assert.equal(actual, 'Тройка');
+    });
+
+	it ('should return `Две пары` for [4, 4, 1, 6, 1]', () =>{
+    	const actual = getPokerHand([4, 4, 1, 6, 1]);
+
+    	assert.equal(actual, 'Две пары');
+    });
+
+	it ('should return `Пара` for [8, 9, 7, 5, 5]', () =>{
+    	const actual = getPokerHand([8, 9, 7, 5, 5]);
+
+    	assert.equal(actual, 'Пара');
+    });
+
+	it ('should return `Наивысшее очко` for [3, 4, 5, 6, 7]', () =>{
+    	const actual = getPokerHand([3, 4, 5, 6, 7]);
+
+    	assert.equal(actual, 'Наивысшее очко');
+    });
+
     // Напишите тесты на ваш замечательный код здесь
 });
