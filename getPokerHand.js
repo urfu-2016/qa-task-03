ea32010dc340ab9036b54b92d614a0b75a71fb0b
AnastasiaBabushkina <nastena.babushkina.96@mail.ru>
2017-04-25 15:55:24 +0500

    правки в getPokerHand

--- getPokerHand.js
+++ getPokerHand.js
@@ -5,27 +5,27 @@
  * @returns {String} название комбинации
  */
 function getPokerHand(dice) {
-    // Напишите ваш замечательный код здесь
-    if (arguments.length !== 1)
-         throw new Error('Неверное число аргументов');
-    if (dice.length !== 5)
-         throw new Error('Было брошено не 5 костей');
-    var setDice = new Set(dice);
-    for (var item of setDice){
+	// Напишите ваш замечательный код здесь
+	if (arguments.length !== 1)
+		 throw new Error('Неверное число аргументов');
+	if (dice.length !== 5)
+		 throw new Error('Было брошено не 5 костей');
+	var setDice = new Set(dice);
+	for (var item of setDice){
 		if (typeof item !== 'number' || isNaN(item))
 			throw new Error('Значение кости не число');
-		if (item <= 0)
-			throw new Error('Значение кости не положительное число');
 		if (!Number.isInteger(item))
 			throw new Error('Значение кости не целое число');
+		if (item <= 0)
+			throw new Error('Значение кости не положительное число');
 		if (item > 6)
 			throw new Error('Значение кости больше 6');
 	}
-	if (setDice.size == 1)
+	if (setDice.size === 1)
 		return 'Покер';
-	if (setDice.size == 5)
+	if (setDice.size === 5)
 		return 'Наивысшее очко';
-	if (setDice.size == 4)
+	if (setDice.size === 4)
 		return 'Пара';
 	else {
 		var QuantityList = getCountOfOccurrences(dice, setDice);
@@ -47,7 +47,7 @@ function getCountOfOccurrences(dice, setDice) {
 	var QuantityList = [];
 	var k = 0;
 	for (var item of setDice){
-		for (var i = 0; i <= 5; i++) {
+		for (var i = 0; i < 5; i++) {
 			if (item == dice[i]){
 				k++;
 			}
@@ -59,3 +59,4 @@ function getCountOfOccurrences(dice, setDice) {
 }
 
 module.exports = getPokerHand;
+getPokerHand([1, 1, 2, 2, 3])
