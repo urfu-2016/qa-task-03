5dc8d6f0e4618db3774fabc0766040d4a1824ec1
CLearERR <berd_gow@mail.ru>
2017-05-02 01:59:22 +0500

    Добавлены проверки: является ли массивом, не является ли пустым вводом, на количество элементов в массиве, является ли элемент массива числом, лежит ли элемент массива в диапазоне от 1 до 6

--- getPokerHand.js
+++ getPokerHand.js
@@ -8,29 +8,64 @@
 
 function getPokerHand(dice) {
     // Напишите ваш замечательный код здесь
-    dice.sort();
+
+    try {
+
+    dice.sort(); 
+
+	} 
+
+    catch (err) {
+
+	res = ('Не массив');
+	return res;
+
+	}
+
+    if (dice.length<5) {
+	res = ('Элементов меньше 5-ти')
+	return res;
+	}
+
+    if (dice.length>5) {
+	res = ('Элементов больше 5-ти')
+	return 'Элементов больше 5-ти';
+	}
+
+
     var num = [1,0,0,0,0];
-    j = 0;
-    for(var i = 1; i<dice.length; i++){
-        if (dice[i]!=dice[j]){
-            j=i;
-        }
-        num[j]++;
-    }
     
- 
+    	j = 0;
+    	for(var i = 1; i<5; i++){
+		if ((isNaN(dice[i])) || (isNaN(dice[j]))) {
+			return 'Массив содержит элемент, не являющийся числом';
+			}
+		if ((dice[i]>6) || (dice[j]>6)) {
+			return 'Массив содержит элемент, больший 6';
+			}
+		if ((dice[i]<1) || (dice[j]<1)) {
+			return 'Массив содержит элемент, меньший 1';
+			}
+	        if (dice[i]!=dice[j]){
+	            j=i;
+	        }
+	        num[j]++;
+	    }
+
+
+	
 
     max1 = -1;
     max2 = -1;
 
-    for(var i = 0; i<num.length; i++){
+    for(var i = 0; i<5; i++){
         if (num[i]>max1){
             max1=num[i];
             num[i]=-1;
         }
     }
 
-    for(var i = 0; i<num.length; i++) {
+    for(var i = 0; i<5; i++) {
         if (num[i]>max2){
             max2=num[i];
             num[i]=-1;
