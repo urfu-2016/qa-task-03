e9b55507bdd9535bee44f3e99e33da2aff2e0a8a
Tsvetkova Valeria <lera.tsvetkova96@gmail.com>
2017-05-03 18:12:19 +0500

    Исправила замечания

--- getPokerHand.js
+++ getPokerHand.js
@@ -14,8 +14,8 @@ function getPokerHand(dice) {
     switch (numbersCount[0]) {
         case 5 : return 'Покер';
         case 4 : return 'Каре';
-        case 3 : return numbersCount[1] == 2 ? 'Фулл хаус' : 'Тройка';
-        case 2 : return numbersCount[1] == 2 ? 'Две пары' : 'Пара';
+        case 3 : return numbersCount[1] === 2 ? 'Фулл хаус' : 'Тройка';
+        case 2 : return numbersCount[1] === 2 ? 'Две пары' : 'Пара';
         case 1 : return 'Наивысшее очко';
     }
 }
@@ -25,15 +25,15 @@ function checkInputData(args) {
     if (args.length != 1)
         throw new Error('На вход должен подаваться только один параметр');
     if (!Array.isArray(args[0]))
-        throw new Error('На вход должен подаваться массив значений');
+        throw new TypeError('На вход должен подаваться массив значений');
     if (args[0].length != correctDiceLength)
-        throw new Error('Массив должен содержать ровно 5 значений');
+        throw new RangeError('Массив должен содержать ровно 5 значений');
     if (Math.min.apply(null,args[0]) < 1 || Math.max.apply(null,args[0]) > 6)
         throw new Error('Значения массива должны находиться в диапазоне от 1 до 6');
     if (args[0].some(function (element) {
         return !Number.isInteger(element);
     }))
-        throw new Error('Значения массива должны являться целыми числами');
+        throw new TypeError('Значения массива должны являться целыми числами');
 }
 
 function getNumbersCount(dice) {
--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -2,68 +2,68 @@ const assert = require('assert');
 const getPokerHand = require('../getPokerHand');
 
 describe('getPokerHand', () => {
-    it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
+    it('should return `Покер` for all same values', () => {
         const actual = getPokerHand([1, 1, 1, 1, 1]);
 
         assert.equal(actual, 'Покер');
     });
-    it('should return `Каре` for [1, 4, 4, 4, 4]', () => {
+    it('should return `Каре` for 4 same values', () => {
         const actual = getPokerHand([1, 4, 4, 4, 4]);
 
         assert.equal(actual, 'Каре');
     });
-    it('should return `Фулл хаус` for [2, 2, 4, 4, 4]', () => {
+    it('should return `Фулл хаус` for pair and triple of same values', () => {
         const actual = getPokerHand([2, 2, 4, 4, 4]);
 
         assert.equal(actual, `Фулл хаус`);
     });
-    it('should return `Тройка` for [3, 3, 3, 1, 4]', () => {
+    it('should return `Тройка` for 3 same values', () => {
         const actual = getPokerHand([3, 3, 3, 1, 4]);
 
         assert.equal(actual, 'Тройка');
     });
-    it('should return `Две пары` for [3, 3, 6, 6, 1]', () => {
+    it('should return `Две пары` for two pairs of same values', () => {
         const actual = getPokerHand([3, 3, 6, 6, 1]);
 
         assert.equal(actual, `Две пары`);
     });
-    it('should return `Пара` for [3, 3, 5, 6, 1]', () => {
+    it('should return `Пара` for two same values', () => {
         const actual = getPokerHand([3, 3, 5, 6, 1]);
 
         assert.equal(actual, `Пара`);
     });
-    it('should return `Наивысшее очко` for [1, 3, 6, 5, 2]', () => {
+    it('should return `Наивысшее очко` for all different values', () => {
         const actual = getPokerHand([1, 3, 6, 5, 2]);
 
         assert.equal(actual, `Наивысшее очко`);
     });
 
-    it('should throw Error for [0, 1, 2, 3, 4]', () => {
+    it('should throw Error for values < 1', () => {
         const cb = () => getPokerHand([0, 1, 2, 3, 4]);
 
         assert.throws(cb, /Значения массива должны находиться в диапазоне от 1 до 6/);
     });
-    it('should throw Error for [7, 1, 2, 3, 4]', () => {
+    it('should throw Error for values > 6', () => {
         const cb = () => getPokerHand([7, 1, 2, 3, 4]);
 
         assert.throws(cb, /Значения массива должны находиться в диапазоне от 1 до 6/);
     });
-    it('should throw Error for [2, 1, 2, 3, 4, 5]', () => {
+    it('should throw RangeError for more than 5 values', () => {
         const cb = () => getPokerHand([2, 1, 2, 3, 4, 5]);
 
         assert.throws(cb, /Массив должен содержать ровно 5 значений/);
     });
-    it('should throw Error for [2, 3, 4, 5]', () => {
+    it('should throw RangeError for less than 5 values', () => {
         const cb = () => getPokerHand([2, 3, 4, 5]);
 
         assert.throws(cb, /Массив должен содержать ровно 5 значений/);
     });
-    it('should throw Error for [1, 2, 3, 4, \'5\']', () => {
+    it('should throw TypeError for NaN values', () => {
         const cb = () => getPokerHand([1, 2, 3, 4, '5']);
 
         assert.throws(cb, /Значения массива должны являться целыми числами/);
     });
-    it('should throw Error for [1, 2, 3, 4, 5.1]', () => {
+    it('should throw TypeError for fractional values', () => {
         const cb = () => getPokerHand([1, 2, 3, 4, 5.1]);
 
         assert.throws(cb, /Значения массива должны являться целыми числами/);
@@ -74,12 +74,12 @@ describe('getPokerHand', () => {
 
         assert.throws(cb, /На вход должен подаваться только один параметр/);
     });
-    it('should throw Error for ([1, 1, 2, 3, 4],[1, 1, 2, 3, 4])', () => {
+    it('should throw Error for more than 1 arguments', () => {
         const cb = () => getPokerHand([1, 1, 2, 3, 4],[1, 1, 2, 3, 4]);
 
         assert.throws(cb, /На вход должен подаваться только один параметр/);
     });
-    it('should throw Error for \'123\'', () => {
+    it('should throw TypeError for not an Array argument', () => {
         const cb = () => getPokerHand('123');
 
         assert.throws(cb, /На вход должен подаваться массив значений/);
