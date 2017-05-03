710a4b26f4545acc72d7e75ffa09dda90614743a
Лилия Шаламова < >
2017-05-03 13:50:39 +0500

    Замечания

--- getPokerHand.js
+++ getPokerHand.js
@@ -7,19 +7,20 @@
 function getPokerHand(dice) {
     // Напишите ваш замечательный код здесь
     checkArray(dice);
-    switch (Object.keys(getRepeat(dice)).length) {
+	const repeats = getRepeat(dice);
+    switch (Object.keys(repeats).length) {
         case 5: 
             return 'Наивысшее очко';
         case 4: 
             return 'Пара';
         case 3:
-            if (getMaxOfArray(Object.keys(getRepeat(dice)).map(
-            function(key) { return getRepeat(dice)[key];})) === 3)
+            if (getMaxOfArray(Object.keys(repeats).map(
+            function(key) { return repeats[key];})) === 3)
                 return 'Тройка';
             return 'Две пары';
         case 2: 
-            if (getMaxOfArray(Object.keys(getRepeat(dice)).map(
-            function(key) { return getRepeat(dice)[key];})) === 4)
+            if (getMaxOfArray(Object.keys(repeats).map(
+            function(key) { return repeats[key];})) === 4)
                 return 'Каре';
             return 'Фулл Хаус';
         default : 
@@ -40,15 +41,10 @@ function checkArray(array) {
     if (array.length !== 5)
         throw new Error("Длина входного массива должна быть равна 5");
     
-    if (!array.every(function(element) {
-    return Number.isInteger(element);
-    }))
-        throw new Error("Элементами массива должны быть числа");
-    
     array.forEach(function(element) {
-        if (!(element > 0 && element < 7))
-            throw new Error("Элементы массива должна быть числами в интервале от 1 до 6");
-        });
+        if (!(element > 0 && element < 7 && Number.isInteger(element) && element ^ 0 === element))
+            throw new Error("Элементами массива должны быть целые числа в интервале от 1 до 6");
+    });
 
 }
 /**
